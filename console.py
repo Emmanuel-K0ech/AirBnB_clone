7458852e33319068ccfcf07043d3e753f798318d
General Manu <kipchumbakoech1107@gmail.com>
2024-02-13 01:05:51 +0300

    create instance

--- console.py
+++ console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This is a custom console for the AirBnB project"""
+from models.base_model import BaseModel
+from models import storage
 import cmd
 
 
@@ -19,6 +21,26 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, args):
         """End Of File - exit program"""
         return True
+
+    def do_create(self, args):
+        """Creates a new instance of BaseModel"""
+        obj_bm = None
+
+        if not args:
+            print("** class name missing **")
+        elif args != 'BaseModel':
+            print("** class doesn't exist **")
+        else:
+            obj_bm = BaseModel()
+            storage.save()
+            print(obj_bm.id)
+
+    def do_show(self, args):
+        """
+        Shows string representation of an instance
+        based on the class name an id of the instance
+        """
+        pass#for now
 
 
 """To run the file as the main program"""
