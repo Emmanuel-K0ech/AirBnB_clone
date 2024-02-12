5a066033eed7af3080ffb816f99dfdc48a355854
General Manu <kipchumbakoech1107@gmail.com>
2024-02-12 23:57:38 +0300

    adding the file storage and __init__ file of models

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory"""
+"""
+Creating a unique File storage instance for Hbnb
+"""
 from models.engine.file_storage import FileStorage
-
 
 storage = FileStorage()
 storage.reload()
